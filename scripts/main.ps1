eab1b6be0dce41decdcb35c93cc8d9fd25a86c2d
Marius Storhaug <marstor@hotmail.com>
2025-03-03 23:26:07 +0100

    Refactor: Sort test results files and enhance output formatting with color coding

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -8,7 +8,7 @@ $repo = $env:GITHUB_REPOSITORY
 $runId = $env:GITHUB_RUN_ID
 $testResultsFolder = New-Item -Path . -ItemType Directory -Name 'TestResults' -Force
 gh run download $runId --repo $repo --pattern *-TestResults --dir TestResults
-$files = Get-ChildItem -Path $testResultsFolder -Recurse -File -Filter *.json
+$files = Get-ChildItem -Path $testResultsFolder -Recurse -File -Filter *.json | Sort-Object Name
 
 LogGroup 'List TestResults files' {
     $files.Name | Out-String
@@ -21,19 +21,18 @@ foreach ($file in $files) {
     $object = $content | ConvertFrom-Json
     $testResults.Add($object)
 
-    # LogGroup $fileName {
-    #     $content | Out-String
-    # }
-
-    LogGroup "$fileName" {
-        $result = [pscustomobject]@{
-            Tests        = [int]([math]::Round(($object | Measure-Object -Sum -Property TotalCount).Sum))
-            Passed       = [int]([math]::Round(($object | Measure-Object -Sum -Property PassedCount).Sum))
-            Failed       = [int]([math]::Round(($object | Measure-Object -Sum -Property FailedCount).Sum))
-            NotRun       = [int]([math]::Round(($object | Measure-Object -Sum -Property NotRunCount).Sum))
-            Inconclusive = [int]([math]::Round(($object | Measure-Object -Sum -Property InconclusiveCount).Sum))
-            Skipped      = [int]([math]::Round(($object | Measure-Object -Sum -Property SkippedCount).Sum))
-        }
+    $result = [pscustomobject]@{
+        Tests        = [int]([math]::Round(($object | Measure-Object -Sum -Property TotalCount).Sum))
+        Passed       = [int]([math]::Round(($object | Measure-Object -Sum -Property PassedCount).Sum))
+        Failed       = [int]([math]::Round(($object | Measure-Object -Sum -Property FailedCount).Sum))
+        NotRun       = [int]([math]::Round(($object | Measure-Object -Sum -Property NotRunCount).Sum))
+        Inconclusive = [int]([math]::Round(($object | Measure-Object -Sum -Property InconclusiveCount).Sum))
+        Skipped      = [int]([math]::Round(($object | Measure-Object -Sum -Property SkippedCount).Sum))
+    }
+    $failed = ($result.Failed -gt 0) -or ($result.NotRun -gt 0) -or ($result.Inconclusive -gt 0)
+    $color = $failed ? $PSStyle.Foreground.Red : $PSStyle.Foreground.Green
+    $reset = $PSStyle.Reset
+    LogGroup "$color$fileName$reset" {
         $result | Format-Table | Out-String
     }
 }
