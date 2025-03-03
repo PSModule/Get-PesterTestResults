2f56a541e4e0928411be9737cff01b6460749a40
Marius Storhaug <marstor@hotmail.com>
2025-03-03 11:41:30 +0100

    🪲 [Enhancement]: Add detailed test results summary and processing in main.ps1

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -13,16 +13,16 @@ $files = Get-ChildItem -Path $testResultsFolder -Recurse -File
 LogGroup 'List TestResults files' {
     $files.Name | Out-String
 }
-$files | Format-Table -AutoSize | Out-String
 
 $allCases = [System.Collections.Generic.List[psobject]]::new()
+$testResults = [System.Collections.Generic.List[psobject]]::new()
 foreach ($file in $files) {
     $fileName = $file.BaseName
+    $xmlDoc = [xml](Get-Content -Path $file.FullName)
     LogGroup $fileName {
         Get-Content -Path $file | Out-String
     }
-    LogGroup "$fileName - Process" {
-        $xmlDoc = [xml](Get-Content -Path $file.FullName)
+    LogGroup "$fileName - Tests" {
         $cases = $xmlDoc.SelectNodes('//test-case') | ForEach-Object {
             [pscustomobject]@{
                 Name           = $_.name
@@ -38,4 +38,33 @@ foreach ($file in $files) {
         $cases | ForEach-Object { $allCases.Add($_) }
         $cases | Format-List | Out-String
     }
+    LogGroup "$fileName - Summary" {
+        $testResultXml = $xmlDoc.'test-results'
+        $testResult = [pscustomobject]@{
+            Name         = $testResultXml.name
+            Total        = $testResultXml.total
+            Errors       = $testResultXml.errors
+            Failures     = $testResultXml.failures
+            NotRun       = $testResultXml.'not-run'
+            Inconclusive = $testResultXml.inconclusive
+            Ignored      = $testResultXml.ignored
+            Skipped      = $testResultXml.skipped
+            Invalid      = $testResultXml.invalid
+            Date         = $testResultXml.date
+            Time         = $testResultXml.time
+        }
+        $testResults.Add($testResult)
+        $testResult | Format-List | Out-String
+    }
+}
+
+[pscustomobject]@{
+    TotalTests        = $allCases.Count
+    TotalErrors       = $testResults | Measure-Object -Sum -Property Errors
+    TotalFailures     = $testResults | Measure-Object -Sum -Property Failures
+    TotalNotRun       = $testResults | Measure-Object -Sum -Property 'NotRun'
+    TotalInconclusive = $testResults | Measure-Object -Sum -Property Inconclusive
+    TotalIgnored      = $testResults | Measure-Object -Sum -Property Ignored
+    TotalSkipped      = $testResults | Measure-Object -Sum -Property Skipped
+    TotalInvalid      = $testResults | Measure-Object -Sum -Property Invalid
 }
