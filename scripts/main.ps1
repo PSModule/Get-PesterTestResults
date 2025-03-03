8363d4f98a3fcc8a23a4025ca266406e7d6c2332
Marius Storhaug <marstor@hotmail.com>
2025-03-03 12:49:43 +0100

    Fix: Ensure test results summary correctly rounds and casts totals to integers

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -60,32 +60,40 @@ foreach ($file in $files) {
 
 $total = [pscustomobject]@{
     Tests        = $allCases.Count
-    Errors       = [math]::Round(($testResults | Measure-Object -Sum -Property Errors).Sum)
-    Failures     = [math]::Round(($testResults | Measure-Object -Sum -Property Failures).Sum)
-    NotRun       = [math]::Round(($testResults | Measure-Object -Sum -Property NotRun).Sum)
-    Inconclusive = [math]::Round(($testResults | Measure-Object -Sum -Property Inconclusive).Sum)
-    Ignored      = [math]::Round(($testResults | Measure-Object -Sum -Property Ignored).Sum)
-    Skipped      = [math]::Round(($testResults | Measure-Object -Sum -Property Skipped).Sum)
-    Invalid      = [math]::Round(($testResults | Measure-Object -Sum -Property Invalid).Sum)
+    Failures     = [int]([math]::Round(($testResults | Measure-Object -Sum -Property Failures).Sum))
+    Errors       = [int]([math]::Round(($testResults | Measure-Object -Sum -Property Errors).Sum))
+    Invalid      = [int]([math]::Round(($testResults | Measure-Object -Sum -Property Invalid).Sum))
+    NotRun       = [int]([math]::Round(($testResults | Measure-Object -Sum -Property NotRun).Sum))
+    Inconclusive = [int]([math]::Round(($testResults | Measure-Object -Sum -Property Inconclusive).Sum))
+    Ignored      = [int]([math]::Round(($testResults | Measure-Object -Sum -Property Ignored).Sum))
+    Skipped      = [int]([math]::Round(($testResults | Measure-Object -Sum -Property Skipped).Sum))
 }
 $total | Format-Table | Out-String
 
+$totalErrors = 0
 if ($total.Failures -gt 0) {
     Write-GitHubError "There are $($total.Failures) failed tests of $($total.Tests) tests"
-    return $_.Failures
+    $totalErrors += $_.Failures
 }
 
 if ($total.Errors -gt 0) {
     Write-GitHubError "There are $($total.Errors) test errors of $($total.Tests) tests"
-    return $_.Errors
+    $totalErrors += $_.Errors
 }
 
 if ($total.Invalid -gt 0) {
     Write-GitHubError "There are $($total.Invalid) invalid test of $($total.Tests) tests"
-    return $_.Invalid
+    $totalErrors += $_.Invalid
 }
 
 if ($total.NotRun -gt 0) {
     Write-GitHubError "There are $($total.NotRun) test not run of $($total.Tests) tests"
-    return $_.NotRun
+    $totalErrors += $_.NotRun
 }
+
+if ($total.Inconclusive -gt 0) {
+    Write-GitHubError "There are $($total.Inconclusive) inconclusive tests of $($total.Tests) tests"
+    $totalErrors += $_.Inconclusive
+}
+
+exit $totalErrors
