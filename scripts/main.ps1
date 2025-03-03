4b4c53474f5275ec10dfcb9c9d722a1807cf6c5e
Marius Storhaug <marstor@hotmail.com>
2025-03-03 11:06:10 +0100

    🪲 [Enhancement]: Improve test results processing and logging in main.ps1

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -3,19 +3,38 @@
 [CmdletBinding()]
 param()
 
-begin {
-    $scriptName = $MyInvocation.MyCommand.Name
-    Write-Debug "[$scriptName] - Start"
-}
+$PSStyle.OutputRendering = 'Ansi'
+$repo = $env:GITHUB_REPOSITORY
+$runId = $env:GITHUB_RUN_ID
+gh run download $runId --repo $repo --pattern *-TestResults
+$files = Get-ChildItem -Path . -Recurse -File
 
-process {
-    try {
-        Write-Output "Hello, $Subject!"
-    } catch {
-        throw $_
+LogGroup 'List TestResults files' {
+    $files.Name | Out-String
+}
+$files | Format-Table -AutoSize | Out-String
+
+$allCases = [System.Collections.Generic.List[psobject]]::new()
+foreach ($file in $files) {
+    $fileName = $file.BaseName
+    LogGroup $fileName {
+        Get-Content -Path $file | Out-String
     }
-}
-
-end {
-    Write-Debug "[$scriptName] - End"
+    LogGroup "$fileName - Process" {
+        $xmlDoc = [xml](Get-Content -Path $file.FullName)
+        $cases = $xmlDoc.SelectNodes('//test-case') | ForEach-Object {
+            [pscustomobject]@{
+                Name           = $_.name
+                Description    = $_.description
+                Result         = $_.result
+                Success        = [bool]($_.success -eq 'True')
+                Time           = [float]$_.time
+                Executed       = [bool]($_.executed -eq 'True')
+                FailureMessage = $_.failure.message
+                StackTrace     = $_.failure.'stack-trace'
+            }
+        }
+        $cases | ForEach-Object { $allCases.Add($_) }
+        $cases | Format-List | Out-String
+    }
 }
