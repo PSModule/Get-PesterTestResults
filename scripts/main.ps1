40fa088421f99a93dbd408314e66edae8ff8f04a
Marius Storhaug <marstor@hotmail.com>
2025-03-23 16:28:16 +0100

    Refactor: Add support for SourceCodeTestSuites, PSModuleTestSuites, and ModuleTestSuites in main.ps1

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -6,6 +6,17 @@ param()
 $PSStyle.OutputRendering = 'Ansi'
 $repo = $env:GITHUB_REPOSITORY
 $runId = $env:GITHUB_RUN_ID
+
+$sourceCodeTestSuites = $env:PSMODULE_GET_PESTERTESTRESULTS_INPUT_SourceCodeTestSuites | ConvertFrom-Json
+$psModuleTestSuites = $env:PSMODULE_GET_PESTERTESTRESULTS_INPUT_PSModuleTestSuites | ConvertFrom-Json
+$moduleTestSuites = $env:PSMODULE_GET_PESTERTESTRESULTS_INPUT_ModuleTestSuites | ConvertFrom-Json
+
+[pscustomobject]@{
+    SourceCodeTestSuites = $sourceCodeTestSuites
+    PSModuleTestSuites   = $psModuleTestSuites
+    ModuleTestSuites     = $moduleTestSuites
+} | Format-List | Out-String
+
 $testResultsFolder = New-Item -Path . -ItemType Directory -Name 'TestResults' -Force
 gh run download $runId --repo $repo --pattern *-TestResults --dir TestResults
 $files = Get-ChildItem -Path $testResultsFolder -Recurse -File -Filter *.json | Sort-Object Name
